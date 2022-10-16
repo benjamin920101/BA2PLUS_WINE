d683fd883ae786157a4d5c1b4698c8e138b71a92
benjamin920101 <47590892+benjamin920101@users.noreply.github.com>
2022-10-17 00:34:53 +0800

    Update admortization_table.sh

--- admortization_table.sh
+++ admortization_table.sh
@@ -1,12 +1,12 @@
 if [[ "$OSTYPE" == "darwin"* ]]; then
 curl https://dl.winehq.org/wine/wine-gecko/2.47.1/wine-gecko-2.47.1-x86.msi
 curl https://dl.winehq.org/wine/wine-gecko/2.47.1/wine-gecko-2.47.1-x86_64.msi
-msiexec wine-gecko-2.47.1-x86.msi
-msiexec wine-gecko-2.47.1-x86_64.msi
+msiexec /i wine-gecko-2.47.1-x86.msi
+msiexec /i wine-gecko-2.47.1-x86_64.msi
 else
 wget https://dl.winehq.org/wine/wine-gecko/2.47.1/wine-gecko-2.47.1-x86.msi
 wget https://dl.winehq.org/wine/wine-gecko/2.47.1/wine-gecko-2.47.1-x86_64.msi
-msiexec wine-gecko-2.47.1-x86.msi
-msiexec wine-gecko-2.47.1-x86_64.msi
+msiexec /i wine-gecko-2.47.1-x86.msi
+msiexec /i wine-gecko-2.47.1-x86_64.msi
 fi
 
